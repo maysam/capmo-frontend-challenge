804b592beaf00de570418fce54932be8df9ddc69
Maysam Torabi <kherad@gmail.com>
2020-03-30 03:44:17 +0430

    Updates

---
+++ precache-manifest.321fc11ec99b42b9e3eefa761b10cfb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ef427d15cd56276a1c101a91e61bf3",
+    "url": "/capmo-frontend-challenge/index.html"
+  },
+  {
+    "revision": "1fd6f6df79739990e96e",
+    "url": "/capmo-frontend-challenge/static/css/main.1b6a9dc0.chunk.css"
+  },
+  {
+    "revision": "2466328bd2ca62f45779",
+    "url": "/capmo-frontend-challenge/static/js/2.293856f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/capmo-frontend-challenge/static/js/2.293856f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd6f6df79739990e96e",
+    "url": "/capmo-frontend-challenge/static/js/main.854b3aa0.chunk.js"
+  },
+  {
+    "revision": "d5f6f1f9cf420f6b7ff6",
+    "url": "/capmo-frontend-challenge/static/js/runtime-main.18a40e5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4405fc654b8004ae17d9f6875728298c.js"
+  "/capmo-frontend-challenge/precache-manifest.321fc11ec99b42b9e3eefa761b10cfb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capmo-frontend-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
